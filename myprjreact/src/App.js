c1a0ce2b9d742813a263a8ff731f44b1e4230d6c
Thanh Lam <lamttFX16122@funix.edu.vn>
2022-04-26 20:22:06 +0700

    Components part 2

--- myprjreact/src/App.js
+++ myprjreact/src/App.js
@@ -1,21 +1,31 @@
 // import logo from './logo.svg';
-import './App.css';
-import {Navbar, NavbarBrand} from 'reactstrap';
-import Menu from './components/MenuComponent';
+import "./App.css";
+import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./components/MenuComponent32";
+import { DISHES } from "./shared/dishes";
+import { Component } from "react";
 
-function App() {
-  return (
- <div>
-      <div className="App">
-     <Navbar dark color="primary">
-       <div className="container">
-         <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-       </div>
-     </Navbar>
-    </div>
-    <Menu></Menu>
- </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    };
+  }
+  render() {
+    return (
+      <div>
+        <div className="App">
+          <Navbar dark color="primary">
+            <div className="container">
+              <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            </div>
+          </Navbar>
+        </div>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ myprjreact/src/components/MenuComponent32.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardText,
+  CardBody,
+  CardTitle,
+} from "reactstrap";
+class MenuComponent32 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedDish: null,
+    };
+  }
+  changeState=dish=>{
+    this.setState({
+        selectedDish:dish
+    });
+  };
+  renderDishEventOnclick = (dish) => {
+    if (dish != null) {
+      return (
+        <Card>
+          <CardImg top src={dish.image} alt={dish.name} />
+          <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+          </CardBody>
+        </Card>
+      );
+    }
+  };
+  render() {
+    // vong lap Dish duoc truyen tu App.js
+    let menu = this.props.dishes.map((value, index) => {
+      return (
+        <div key={value.id} className="col-12 col-md-5 m-1">
+          <Card onClick={()=>this.changeState(value)}>
+            <CardImg width="100%" src={value.image} alt={value.name} />
+            {/*image*/}
+            <CardImgOverlay>
+              <CardTitle>{value.name}</CardTitle>
+            </CardImgOverlay>
+          </Card>
+        </div>
+      );
+    });
+    return (
+      <div className="container">
+        <div className="row">{menu}</div>
+        {
+          /* Change state */
+          this.renderDishEventOnclick(this.state.selectedDish)
+        }
+        <div className="row">
+          <div className="col-12 col-md-5 m-1">{}</div>
+        </div>
+      </div>
+    );
+  }
+}
+export default MenuComponent32;
