32094da0afe70c8dabbc13c0c83620162b798220
Thanh Lam <lamttFX16122@funix.edu.vn>
2022-04-26 17:02:18 +0700

    Component part 1

--- myprjreact/src/App.js
+++ myprjreact/src/App.js
@@ -1,16 +1,20 @@
 // import logo from './logo.svg';
 import './App.css';
 import {Navbar, NavbarBrand} from 'reactstrap';
+import Menu from './components/MenuComponent';
 
 function App() {
   return (
-    <div className="App">
+ <div>
+      <div className="App">
      <Navbar dark color="primary">
        <div className="container">
          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
        </div>
      </Navbar>
     </div>
+    <Menu></Menu>
+ </div>
   );
 }
 
