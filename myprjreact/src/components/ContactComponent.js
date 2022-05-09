a0a72fbb02536ff28e6b2e538cb39fd2c624a7c4
Thanh Lam <lamttFX16122@funix.edu.vn>
2022-05-09 14:21:57 +0700

    Controlled Form Validation

--- myprjreact/src/components/ContactComponent.js
+++ myprjreact/src/components/ContactComponent.js
@@ -2,32 +2,83 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Breadcrumb, BreadcrumbItem } from "reactstrap";
 class Contact extends Component {
-    constructor(props) {
-    	super(props);
-        this.state={
-            firstName:"",
-            lastName:"",
-            contactTel:"",
-            email:"",
-            ckbContact:false,
-            sltTel:"Tel.",
-            feedBack:''
-        }
-    }
-    onChange=(e)=>{
-        const target=e.target;
-        const name=target.name;
-        const value=target.type==="checkbox"? target.checked: target.value;
-        this.setState({
-            [name]:value
-        })
-    }
-    onSubmit=(e)=>{
-        e.preventDefault();
-        console.log("Current state is: "+JSON.stringify(this.state));
-        alert("Current state is: "+JSON.stringify(this.state));
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: "",
+      lastName: "",
+      contactTel: "",
+      email: "",
+      ckbContact: false,
+      sltTel: "Tel.",
+      feedBack: "",
+      touched: {
+        firstName: false,
+        lastName: false,
+        contactTel: false,
+        email: false,
+      },
+    };
+  }
+  onChange = (e) => {
+    const target = e.target;
+    const name = target.name;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    });
+  };
+  onSubmit = (e) => {
+    e.preventDefault();
+    console.log("Current state is: " + JSON.stringify(this.state));
+    alert("Current state is: " + JSON.stringify(this.state));
+  };
+  onBlur = (field) => (e) => {
+    this.setState({
+      touched: {
+        ...this.state.touched,
+        [field]: true,
+        // touched: { ...this.state.touched, [field]: true }
+      },
+    });
+  };
+
+  validate = (firstName, lastName, contactTel, email) => {
+    const { touched } = this.state;
+    const err = {
+      firstName: "",
+      lastName: "",
+      contactTel: "",
+      email: "",
+    };
+    // first name
+    // console.log(touched);
+    if (touched.firstName && firstName.length < 3)
+      err.firstName = "First Name should be >=3 characters";
+    else if (touched.firstName && firstName.length > 10)
+      err.firstName = "First Name should be <=10 characters";
+
+    // last name
+    if (touched.lastName && lastName.length < 3)
+      err.lastName = "Last Name should be >=3 characters";
+    else if (touched.lastName && lastName.length > 10)
+      err.lastName = "Last Name should be <=10 characters";
+
+    // contactTel
+    const reg = /^\d+$/;
+    if (touched.contactTel && !reg.test(contactTel))
+      err.contactTel = "Tel. Number should contain only numbers";
+
+    //email
+    var regEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    if (touched.email && !regEmail.test(email))
+      err.email = "Invalid email address";
+    return err;
+  };
+
   render() {
+    const { firstName, lastName, contactTel, email } = this.state;
+    const err = this.validate(firstName, lastName, contactTel, email);
     return (
       <div className="container">
         <div className="row">
@@ -95,18 +146,69 @@ class Contact extends Component {
             <form onSubmit={this.onSubmit}>
               <div className="mb-3">
                 <label className="form-label">First Name</label>
-                <input type="text" className="form-control" name="firstName" onChange={this.onChange}/>
+                <input
+                  type="text"
+                  className={
+                    err.firstName === ""
+                      ? "form-control"
+                      : "form-control is-invalid"
+                  }
+                  name="firstName"
+                  onBlur={this.onBlur("firstName")}
+                  onChange={this.onChange}
+                />
+                <div className="invalid-feedback">{err.firstName}</div>
+
                 <label className="form-label">Last Name</label>
-                <input type="text" className="form-control" name="lastName" onChange={this.onChange}/>
+                <input
+                  type="text"
+                  className={
+                    err.lastName === ""
+                      ? "form-control"
+                      : "form-control is-invalid"
+                  }
+                  name="lastName"
+                  onBlur={this.onBlur("lastName")}
+                  onChange={this.onChange}
+                />
+                <div className="invalid-feedback">{err.lastName}</div>
                 <label className="form-label">Contact Tel</label>
-                <input type="text" className="form-control" name="contactTel" onChange={this.onChange}/>
+                <input
+                  type="text"
+                  className={
+                    err.contactTel === ""
+                      ? "form-control"
+                      : "form-control is-invalid"
+                  }
+                  name="contactTel"
+                  onBlur={this.onBlur("contactTel")}
+                  onChange={this.onChange}
+                />
+                <div className="invalid-feedback">{err.contactTel}</div>
                 <label className="form-label">Email</label>
-                <input type="email" className="form-control" name="email" onChange={this.onChange}/>
+                <input
+                  type="email"
+                  className={
+                    err.email === ""
+                      ? "form-control"
+                      : "form-control is-invalid"
+                  }
+                  name="email"
+                  onBlur={this.onBlur("email")}
+                  onChange={this.onChange}
+                />
+                <div className="invalid-feedback">{err.email}</div>
               </div>
               <div className="row">
                 <div className="col-10">
                   <div className="mb-3 form-check">
-                    <input type="checkbox" className="form-check-input" name="ckbContact" value={this.state.ckbContact} onChange={this.onChange}/>
+                    <input
+                      type="checkbox"
+                      className="form-check-input"
+                      name="ckbContact"
+                      value={this.state.ckbContact}
+                      onChange={this.onChange}
+                    />
                     <label className="form-check-label">
                       May we contact you?
                     </label>
@@ -117,7 +219,8 @@ class Contact extends Component {
                     className="form-select"
                     aria-label="Default select example"
                     name="sltTel"
-                    value={this.state.sltTel} onChange={this.onChange}
+                    value={this.state.sltTel}
+                    onChange={this.onChange}
                   >
                     <option value={"Tel."}>Tel.</option>
                     <option value={"Email"}>Email</option>
@@ -126,9 +229,7 @@ class Contact extends Component {
               </div>
 
               <div className="mb-3">
-                <label  className="form-label">
-                 Your Feedback
-                </label>
+                <label className="form-label">Your Feedback</label>
                 <textarea
                   className="form-control"
                   rows="3"
