25f3470d3159743f2df4826e1706f8bb2fc763fa
Thanh Lam <lamttFX16122@funix.edu.vn>
2022-05-17 10:42:00 +0700

    add Combining Reducers of Lab_8

---
+++ myprjreact/src/redux/promotions.js
@@ -0,0 +1,6 @@
+import { PROMOTIONS } from "../shared/promotions";
+export const Promotions = (state = PROMOTIONS, action) => {
+    switch (action.state) {
+        default: return state;
+    }
+}
