319176d29d770d77938490d285fab8b46796a453
Thanh Lam <lamttFX16122@funix.edu.vn>
2022-05-17 10:45:12 +0700

    comment tag React.StrictMode of file Index.js and ReComplete Combining Reducers

--- myprjreact/src/index.js
+++ myprjreact/src/index.js
@@ -9,9 +9,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/js/all.js';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
