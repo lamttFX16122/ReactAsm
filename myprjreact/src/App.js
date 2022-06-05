766d33a75fa98fe1d3a955585f5f6ad5314b49f5
Thanh Lam <lamttFX16122@funix.edu.vn>
2022-06-05 23:52:15 +0700

    Add React in line import react {Componet} for App.js and add style for MenuComponent at App.css

--- myprjreact/src/App.js
+++ myprjreact/src/App.js
@@ -1,20 +1,21 @@
 // import logo from './logo.svg';
-import './App.css';
-import {Navbar, NavbarBrand} from 'reactstrap';
-import Menu from './components/MenuComponent';
+import React from "react";
+import "./App.css";
+import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./components/MenuComponent";
 
 function App() {
   return (
- <div>
+    <div>
       <div className="App">
-     <Navbar dark color="primary">
-       <div className="container">
-         <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-       </div>
-     </Navbar>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+      </div>
+      <Menu />
     </div>
-    <Menu></Menu>
- </div>
   );
 }
 
